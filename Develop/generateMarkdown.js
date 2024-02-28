d9599ffdc38f9df07869596935b2ce9f554d6646
sara-hines <90005274+sara-hines@users.noreply.github.com>
2024-02-27 17:22:49 -0700

    In index.js: require fs module, inquirer package, and generateMarkdown.js module; add questions array, inquirer functionality, writeFile method of the fs module; call the function to generate the user's customized README content using functionality provided by the exported generateMarkdown.js module. In generatedMarkdown.js: add the functions to render the license badge, license link, and license section; generate the user's customized README content in function generateMarkdown; export the generateMarkdown function.

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,4 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(answers) {
   if (answers.license === "MIT License") {
